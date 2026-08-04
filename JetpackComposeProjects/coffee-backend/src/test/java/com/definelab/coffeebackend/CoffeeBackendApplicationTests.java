5ce5773bfdb2e7e9a0161f0b95fb2b1dc0879335
ashutoshutane <ashutoshutane453@gmail.com>
2026-08-04 20:29:05 +0530

    Database

---
+++ JetpackComposeProjects/coffee-backend/src/test/java/com/definelab/coffeebackend/CoffeeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.definelab.coffeebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
